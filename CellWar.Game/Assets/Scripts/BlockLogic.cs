ea261cdba004fc8fa3f603ce44bc751f691fd369
BennyCui <bennycui99@gmail.com>
2019-07-14 19:17:49 +0800

    update Basicscene
    和model做衔接

--- CellWar.Game/Assets/Scripts/BlockLogic.cs
+++ CellWar.Game/Assets/Scripts/BlockLogic.cs
@@ -1,12 +1,17 @@
 ﻿using UnityEngine;
-
+using CellWar.Model.Map;
+using System.Collections.Generic;
 
 public class BlockLogic : MonoBehaviour
 {
+    List<GameObject> neighbour = new List<GameObject>();
+
+    Block mBlock = new Block();
     // Start is called before the first frame update
     void Start()
     {
-        
+        mBlock.BlockType = Block.Type.Normal;
+        mBlock.Strains.
     }
 
     // Update is called once per frame
