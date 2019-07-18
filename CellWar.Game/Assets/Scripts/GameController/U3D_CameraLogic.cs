fe4633c8df92f58a6ef4fefd94434558d9bacf91
BennyCui <bennycui99@gmail.com>
2019-07-18 23:32:25 +0800

    Fix Camera move
    添加了camera移动的范围

--- CellWar.Game/Assets/Scripts/GameController/U3D_CameraLogic.cs
+++ CellWar.Game/Assets/Scripts/GameController/U3D_CameraLogic.cs
@@ -4,27 +4,44 @@ using UnityEngine;
 
 public class U3D_CameraLogic : MonoBehaviour {
     float cameraMove = CellWar.Model.Settings.Camera.MoveSpeed;
+    [SerializeField]
+    float CameraXMax;
+
+    [SerializeField]
+    float CameraXMin;
+
+    [SerializeField]
+    float CameraYMax;
+
+    [SerializeField]
+    float CameraYMin;
+
+    [SerializeField]
+    float CameraZMax;
+
+    [SerializeField]
+    float CameraZMin;
 
     // Update is called once per frame
     void Update() {
         Vector3 newPosition = transform.position;
 
-        if( Input.mousePosition.x >= Screen.width - 10 ) {
+        if( Input.mousePosition.x >= Screen.width - 10 && Camera.main.transform.position.x <= CameraXMax) {
             newPosition = new Vector3( Camera.main.transform.position.x + cameraMove, Camera.main.transform.position.y, Camera.main.transform.position.z );
-        } else if( Input.mousePosition.x <= 10 ) {
+        } else if( Input.mousePosition.x <= 10 && Camera.main.transform.position.x >= CameraXMin) {
             newPosition = new Vector3( Camera.main.transform.position.x - cameraMove, Camera.main.transform.position.y, Camera.main.transform.position.z );
         }
-        if( Input.mousePosition.y >= Screen.height - 10 ) {
+        if( Input.mousePosition.y >= Screen.height - 10 && Camera.main.transform.position.z <= CameraYMax) {
             newPosition = new Vector3( Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z + cameraMove );
-        } else if( Input.mousePosition.y <= 10 ) {
+        } else if( Input.mousePosition.y <= 10 && Camera.main.transform.position.z >= CameraYMin) {
             newPosition = new Vector3( Camera.main.transform.position.x, Camera.main.transform.position.y, Camera.main.transform.position.z - cameraMove );
         }
 
         /// 处理鼠标滚轮
-        if( Input.GetAxis( "Mouse ScrollWheel" ) > 0 ) {
+        if( Input.GetAxis( "Mouse ScrollWheel" ) > 0 && Camera.main.transform.position.y >= CameraZMin) {
             newPosition = new Vector3( Camera.main.transform.position.x, Camera.main.transform.position.y - 1, Camera.main.transform.position.z );
         }
-        if( Input.GetAxis( "Mouse ScrollWheel" ) < 0 ) {
+        if( Input.GetAxis( "Mouse ScrollWheel" ) < 0 && Camera.main.transform.position.y <= CameraZMax) {
             newPosition = new Vector3( Camera.main.transform.position.x, Camera.main.transform.position.y + 1, Camera.main.transform.position.z );
         }
 
